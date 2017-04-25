916f63de47d2b1d234847bad73d11561f7e9fbd8
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2017-04-25 01:19:34 -0500

    Use format() to make strings clearer and add linspace por k1 values

--- k1Iteration.py
+++ k1Iteration.py
@@ -9,14 +9,11 @@ mif_file = os.path.abspath('/home/carlosmiguelpatino/Documents/FerromagneticReso
 
 # make our list of sizes that we will loop through
 # in nm as our mif file converts to metres.
-k1_values = [100, 200]
+k1_values = np.linspace(-4.5e3, -4.5e3*1.2, 5)
 
 for k1 in k1_values:
-    oommf_string = 'tclsh' + ' ' + path_oommf + \
-    ' boxsi -parameters "K1_iter %s" %s' % \
-     (k1, mif_file)
-    print(oommf_string)
-    output_file_name = 'pulse_K1='+str(k1)
-    odt_to_csv = 'tclsh '+ path_oommf + ' odtcols -t csv <'+ output_file_name+ '.odt >' + output_file_name+ '.csv'
+    oommf_string = 'tclsh {0} boxsi -parameters "K1_iter {1:.0f}" {2}'.format(path_oommf, k1, mif_file)
+    output_file_name = 'pulse_K1={0:.0f}'.format(k1)
+    odt_to_csv = 'tclsh {0} odtcols -t csv <{1}.odt >{1}.csv'.format(path_oommf, output_file_name)
     subprocess.call(oommf_string, shell=True)
     subprocess.call(odt_to_csv, shell=True)
