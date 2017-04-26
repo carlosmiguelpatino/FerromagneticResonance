7e31bbc84823d3b25033e420dae27ef94f4730ab
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2017-04-25 23:38:51 -0500

    separate data in directory and remove .odt

--- k1Iteration.py
+++ k1Iteration.py
@@ -16,6 +16,7 @@ def k1Iteration(initial_value, end_value, n_points):
     for k1 in k1_values:
         oommf_string = 'tclsh {0} boxsi -parameters "K1_iter {1:.0f}" {2}'.format(path_oommf, k1, mif_file)
         output_file_name = 'pulse_K1={0:.0f}'.format(k1)
-        odt_to_csv = 'tclsh {0} odtcols -t csv <{1}.odt >{1}.csv'.format(path_oommf, output_file_name)
+        odt_to_csv = 'tclsh {0} odtcols -t csv <./data/{1}.odt >./data/{1}.csv'.format(path_oommf, output_file_name)
         subprocess.call(oommf_string, shell=True)
         subprocess.call(odt_to_csv, shell=True)
+        subprocess.call('rm ./data/*.odt', shell=True)
